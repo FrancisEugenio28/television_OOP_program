5a9670073e82380d02a0a5a3ae5424f4142900f6
FrancisEugenio| BSCPE 1-5 <128889785+FrancisEugenio28@users.noreply.github.com>
2023-05-13 20:09:24 +0800

    Pseudocode so far

---
+++ Television.py
@@ -0,0 +1,15 @@
+#Francis Niño A. Eugenio
+#BSCPE 1-5 | Object Oriented Programing
+#Assignment 6
+
+# create class for television
+# Television ON
+# Television OFF
+# Determine the current channel
+# Setting the channel to another channel
+# Determine the current volume of the TV
+# Setting the volume to another VolumeLevel
+# Channel UP
+# Channel DOWN
+# Volume UP
+# Volume DOWN
