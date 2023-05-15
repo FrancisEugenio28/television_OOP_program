50899675124fae4164282b39dcf1f2393bbf8762
FrancisEugenio| BSCPE 1-5 <128889785+FrancisEugenio28@users.noreply.github.com>
2023-05-15 11:16:40 +0800

    Doing some editing through the function about on and off

--- Television.py
+++ Television.py
@@ -11,15 +11,17 @@ class television:
         self.volume = volume
         
 # Television ON
-    def status_ON(self):
-        print("The TV is", self.status)
-# Television OFF
-    def status_OFF(self):
-        print("The TV is", self.status)
+    def TV_status(self):
+        if self.status == 'ON':
+            print("The TV is", self.status)
+        else:
+            print("The TV is", self.status)
 # Determine the current channel
     def get_channel(self):
-        print ("Your currently at the channel num.", self.channel)
+        print ("You're currently at the channel num.", self.channel)
 # Setting the channel to another channel
+    def set_channel(self):
+        print ("You're currently now at the channel num.", self.channel)
 # Determine the current volume of the TV
 # Setting the volume to another VolumeLevel
 # Channel UP
@@ -27,6 +29,7 @@ class television:
 # Volume UP
 # Volume DOWN
 
-TV = television('OFF', 2, 5)
-TV.status_ON()
-TV.get_channel()
+TV1 = television('ON', 2, 5)
+TV1.TV_status()
+TV1.get_channel()
+
