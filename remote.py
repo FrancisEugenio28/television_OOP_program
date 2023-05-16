0bac9fd06998f66c7b2e670d5f13203777381c0d
FrancisEugenio| BSCPE 1-5 <128889785+FrancisEugenio28@users.noreply.github.com>
2023-05-17 07:23:39 +0800

    Now we gonna create a new file which will be the one will call all the function to our source code

---
+++ remote.py
@@ -0,0 +1,9 @@
+from Television import *
+from tkinter import *
+
+root = Tk()
+root.title('Remote')
+root.geometry('200x280')
+root.resizable(0,0)
+root.configure(background='black')
+root.mainloop()
