5274d876157a312731512d218ee60349d0d5d82f
FrancisEugenio| BSCPE 1-5 <128889785+FrancisEugenio28@users.noreply.github.com>
2023-05-17 07:30:58 +0800

    Im planning to create a remote for the Television and here is my pseudocode so far

--- remote.py
+++ remote.py
@@ -6,4 +6,14 @@ root.title('Remote')
 root.geometry('200x280')
 root.resizable(0,0)
 root.configure(background='black')
+
+# we create a buttons which calls the functions individually
+    # On and Off
+    # Get channel
+    # Get volume
+    # Channel up
+    # Channel down
+    # Volume up
+    # Volume down
+
 root.mainloop()
